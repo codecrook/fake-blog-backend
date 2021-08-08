8b8681820cda2ab36194a5b6548fc3d34b0466b0
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-08-09 00:24:31 +0530

    ADD: sample post request endpoint

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => res.send('Hello!'));
+app.post('/hello', (req, res) => res.send(`Hello, ${req.body.name}!`));
 
 
 
